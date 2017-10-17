27d4b9e7e3ecca634eac00f7fdd824f670c9d279
Alice <aliceedwards1999@live.co.uk>
2017-10-17 14:36:27 +0100

    Learn the difference between past and present tense plz, also added doors and clock as rooms

--- map.py
+++ map.py
@@ -5,12 +5,14 @@ room_start = {
 
     "description":
     """You are in the dank and dimly lit room where you
-first awoke. The room contains two doors along with a
+first awoke. The only light is cast from a daunting red
+display on the wall. The room contains two doors along with a
 table and the chair you slept on. One of the doors is
 green and the other is gold. Entering back into this
-room makes you shiver, the atmosphere not helped by the
-digital clock with a red display ominously ticking down
-on the wall. You need to escape. And fast.""",
+room makes you shiver. You take another look at the display
+with its red lights gleaming at you worsening the atmosphere.
+
+You need to escape. Fast.""",
 
     "exits": {"table": "Table", "chair": "Chair"},
 
@@ -22,9 +24,9 @@ room_table = {
 
     "description":
     """Out of place in the dimly lit room, the hardwood
-table appeared high end and bespoke. On its surface
-were 4 coloured sheets of paper with writing on them.
-One was blue, one red, one yellow and one green.""",
+table appears up-market and bespoke. On its surface
+are 4 coloured sheets of paper with writing on them.
+One is blue, one red, one yellow and one green.""",
 
     "exits": {"back": "Start"},
 
@@ -45,8 +47,51 @@ looks light enough that it could easily be picked up if you should need to.""",
     "items": []
 }
 
+room_gold_door = {
+    "name": "the gold door",
+
+    "description":
+    """This tall towering door looms down on you with hopes of escape or truth. As you glance at
+    it you cannot help but feel frustrated that it is not a fire exit, why can't you just leave
+    already? 
+
+    It seems it requires a username and password to escape."""
+
+    "exits": {"back": "Start"},
+
+    "items": []
+}
+
+room_green_door = {
+    "name": "the green door"
+
+    "description":
+    """The green door seems like it belongs in this dimly lit room, its wood seems aged, yet 
+    you are well aware you wouldn't be able to break it down. Curiosity leaves you craving 
+    to know what awaits you on the other side.
+
+    It appears the only way through is to use a key..."""
+
+    "exits": {"back": "Start"},
+
+    "items": []
+}
+
+room_clock = {
+    "name": "the clock"
+
+    "description":
+    """The red display of this clock is very ominous. It's decreasing value seems to only
+    mean impending doom."""
+
+    "exits" {"back": "Start"}
+
+    "items": []
+}
 rooms = {
     "Start": room_start,
     "Table": room_table,
-    "Chair": room_chair
+    "Chair": room_chair,
+    "Gold Door": room_gold_door
+    "Green Door": room_green_door
 }
