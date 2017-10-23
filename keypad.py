eecd8e799e875d2bc859828ebb0a4c2ce4bec8eb
CallumHaine <32871286+CallumHaine@users.noreply.github.com>
2017-10-23 17:04:43 +0100

    Add files via upload

---
+++ keypad.py
@@ -0,0 +1,34 @@
+def keycode():
+	keycode = input('Please enter 4 digit pin')
+	print("""t888888888888888888888888888888888888888888888888t
+8                                                8
+8                                                8
+8                                                8
+8   .\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%\%:   8
+8   ;8                                      @S   8
+8   ;8                                      @%   8
+8   t8                                      @%   8
+8   ;8 Enter keycode:  """+ keycode + """                 @%   8
+8   ;8                                      @%   8
+8   t8                                      @%   8
+8   :8SXSXSXSSXSXSXSSXSXXSXSXSXSXSXXSXXSXSXS8;   8
+8                                                8
+8   :X8888888888Xt:@8888888888@t;@8888888888Xt   8
+8   ;8          XS;8          XS;8.         @S   8
+8   ;8    1     @%;8     2     @%;     3    @%   8
+8   t8          XS:8          X%;8.         XS   8
+8   :8@XXXXXX@X@8t:8@XX@XX@XXX8t:8@XXX@X@XXX8t   8
+8   ;8          %\%\          %\%\          8%   8
+8   ;8     4    @%;8     5    .@%;    6     @%   8
+8   ;8          @%;8          XS;8          @%   8
+8   ;8;;;;;;;t;;8S:8;;t;;;;t;t8%;8;t;t;;t;t;8S   8
+8    ;ttttttttttt. ;ttttttttttt. ;ttttttttttt.   8
+8   :@8888888888@t:@88888888888t:@88888888888t   8
+8   ;8          @%;8          @%;8          @S   8
+8   ;8     7    XS;8     8    @%;8     9    @%   8
+8   t8          X%;8          XS;8          XS   8
+8   :8XXSXSXSXSX8t:8XSXSXSXSXS8t:8XXSXSXXSXX8t   8
+8                                                8
+XS%\S\%\S\%\S\%\S\S%\%\S\%\S\%\S\%\SS%\S\%\S\%\SS@""")
+	
+keycode()
